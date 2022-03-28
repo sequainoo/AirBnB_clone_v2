7b9fd8f70ecc27605b248448ed98060abbea522b
sequainoo <wazatoyung@gmail.com>
2022-03-28 08:41:36 -0700

    Fixed symbolic to root tto allow static serving

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,28 +1,37 @@
 #!/usr/bin/env bash
-# script sets up a web server for deployement of web_static
+# setup web-server for web_static deployment
+# create /data/web_static/releases/test/ and /data/web_static/shared/
+# create a fake file in /data/web_static/releases/test/inde.html
 
-# install nginx
+# install nginx if not installed
 sudo apt-get update
 sudo apt-get -y upgrade
-sudo apt-get -y install nginx
+sudo apt-get -y install
 
-# setup dir /data/ if not exist
+# setup dir structure
 sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
 
-# a fake html file in test
-echo 'Fake html doc content' | sudo tee /data/web_static/releases/test/index.html
+# setup faek html
+echo 'fake content' | sudo tee /data/web_static/releases/test/index.html
 
-# create a symbolic link to /test/
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+# create a symlink to /data/web_static/releases/test as current in /data/web_static/
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
 
-# change ownership of /data/ and all subcontents to ubuntu
+# change ownership of /data/ to ubuntu user and group
+# -h --no-dereference in the case of symlink
 sudo chown -hR ubuntu:ubuntu /data/
 
-# add to nginx configuration to serve from /data/web_static/current/ when
-# requested /hbnb_static: use alias
-CONFIG="\\\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n"
+# update nginx config: /etc/nginx/sites-available/default
+# to serve the content of /data/web_static/current/ to hbnb_static
+# eg: http://mydomain.tech/hbnb_static
+# config:
+#       location /hbnb_static/ {
+#               alias /data/web_static/current/;
+#       }
+CONFIG="\\\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}"
+
 sudo sed -i "/server_name _/a$CONFIG" /etc/nginx/sites-available/default
 
-# restart nginx
-sudo service nginx start
+# restart nginx after config
+sudo service nginx restart
 
