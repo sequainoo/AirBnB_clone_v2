a8c530cb7cbcfe422b2aaba6282a8c77d6b5e05c
sequainoo <wazatoyung@gmail.com>
2022-03-28 01:16:25 -0700

    Add 0-setup_web_static.sh

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+# script sets up a web server for deployement of web_static
+
+# install nginx
+sudo apt-get -y install nginx
+
+# setup dir /data/ if not exist
+sudo mkdir -p /data/web_static/releases/test/ /data/web_static/shared
+
+# a fake html file in test
+echo 'Fake html doc content' | sudo tee /data/web_static/releases/test/index.html
+
+# create a symbolic link to /test/
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+# change ownership of /data/ and all subcontents to ubuntu
+sudo chown -hR ubuntu:ubuntu /data/
+
+# add to nginx configuration to serve from /data/web_static/current/ when
+# requested /hbnb_static: use alias
+CONFIG="\\\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n"
+sudo sed -i "/server_name _/a$CONFIG" /etc/nginx/sites-available/default
+
+# restart nginx
+sudo service nginx start
+
