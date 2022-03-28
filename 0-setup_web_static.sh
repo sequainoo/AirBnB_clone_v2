dc6700a94f700b6fa2b27202abab23c252f09a21
sequainoo <wazatoyung@gmail.com>
2022-03-28 09:14:51 -0700

    edit

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,36 +1,27 @@
 #!/usr/bin/env bash
-# setup web-server for web_static deployment
-# create /data/web_static/releases/test/ and /data/web_static/shared/
-# create a fake file in /data/web_static/releases/test/inde.html
+# script sets up a web server for deployement of web_static
 
-# install nginx if not installed
+# install nginx
 sudo apt-get update
 sudo apt-get -y upgrade
-sudo apt-get -y install
+sudo apt-get -y install nginx
 
-# setup dir structure
+# setup dir /data/ if not exist
 sudo mkdir -p /data/web_static/releases/test/ /data/web_static/shared
 
-# setup faek html
-echo 'fake content' | sudo tee /data/web_static/releases/test/index.html
+# a fake html file in test
+echo 'Fake html doc content' | sudo tee /data/web_static/releases/test/index.html
 
-# create a symlink to /data/web_static/releases/test as current in /data/web_static/
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+# create a symbolic link to /test/
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
 
-# change ownership of /data/ to ubuntu user and group
-# -h --no-dereference in the case of symlink
+# change ownership of /data/ and all subcontents to ubuntu
 sudo chown -hR ubuntu:ubuntu /data/
 
-# update nginx config: /etc/nginx/sites-available/default
-# to serve the content of /data/web_static/current/ to hbnb_static
-# eg: http://mydomain.tech/hbnb_static
-# config:
-#       location /hbnb_static/ {
-#               alias /data/web_static/current/;
-#       }
-CONFIG="\\\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}"
-
+# add to nginx configuration to serve from /data/web_static/current/ when
+# requested /hbnb_static: use alias
+CONFIG="\\\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n"
 sudo sed -i "/server_name _/a$CONFIG" /etc/nginx/sites-available/default
 
-# restart nginx after config
+# restart nginx
 sudo service nginx start
