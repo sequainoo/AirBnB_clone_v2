a7122b9b2ed6df1578df3ebe7390761531ba9d9a
sequainoo <wazatoyung@gmail.com>
2022-04-11 03:04:32 -0700

    Add 4-number_route.py

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+'''starts a flask web application.'''
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    '''eturns Hello HBNB!.'''
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    '''Returns HBNB.'''
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    '''Return C followed by value of text: space replace underscore.'''
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_is_cool(text='is cool'):
+    '''Handles /python/(<text>).'''
+    return 'Python {}'.format(text.replace('_', ' '))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    '''displays n is anumber only i it is an integer.'''
+    return '{:d} is number'.format(n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
