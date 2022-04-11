930cf483613120868a3f9d378495f4424bc6d6e3
sequainoo <wazatoyung@gmail.com>
2022-04-11 02:56:52 -0700

    Add 3-python_route.py

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -23,5 +23,12 @@ def c_is_fun(text):
     return 'C {}'.format(text.replace('_', ' '))
 
 
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_is_cool(text='is cool'):
+    '''Handles /python/(<text>).'''
+    return 'Python {}'.format(text.replace('_', ' '))
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
